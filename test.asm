f6520c8e23e6974cb3994a9dd1c39112b0c3db88
Philip Solobay <maximus242@gmail.com>
2024-07-22 21:32:26 -0600

    try fixing hexdump

--- test.asm
+++ test.asm
@@ -1,17 +1,17 @@
 section .data
-    buffer1 db "12345678123456781234567812345678"
-    buffer2 db "87654321876543218765432187654321"
-    result  times 32 db 0
-    multiplier dq 0x40000000, 0x40000000, 0x40000000, 0x40000000, 0x40000000, 0x40000000, 0x40000000, 0x40000000
+buffer1 db '12345678123456781234567812345678'
+buffer2 db '87654321876543218765432187654321'
+result times 32 db 0
+multiplier db 40h, 00h, 00h, 00h, 40h, 00h, 00h, 00h, 40h, 00h, 00h, 00h, 40h, 00h, 00h, 00h
 
 section .text
-    global _start
+global _start
 
 _start:
     ; Load addresses of buffers into registers
-    lea rdi, [buffer1]
-    lea rsi, [buffer2]
-    lea rdx, [result]
+    lea rdi, [rel buffer1]
+    lea rsi, [rel buffer2]
+    lea rdx, [rel result]
 
     ; Load data from buffers into ymm registers
     vmovaps ymm0, [rdi]
@@ -20,19 +20,19 @@ _start:
     ; Add the two buffers
     vaddps ymm2, ymm0, ymm1
 
-    ; Multiply the result by a constant (e.g., 2.0)
-    lea rcx, [multiplier]
+    ; Multiply the result by a constant (2.0)
+    lea rcx, [rel multiplier]
     vmovaps ymm3, [rcx]
     vmulps ymm2, ymm2, ymm3
 
     ; Store the result back into the result buffer
     vmovaps [rdx], ymm2
 
-    ; Zero out the result buffer
+    ; Zero out the result buffer (next 16 bytes)
     vxorps ymm2, ymm2, ymm2
-    vmovaps [rdx + 32], ymm2
+    vmovaps [rdx+16], ymm2
 
     ; Exit
-    mov eax, 60          ; syscall: exit
-    xor edi, edi         ; status: 0
+    mov eax, 60   ; syscall: exit
+    xor edi, edi  ; status: 0
     syscall
