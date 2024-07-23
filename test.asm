5352d40b6d29cba407b8639ad6bf63d29457d022
Philip Solobay <maximus242@gmail.com>
2024-07-22 21:24:47 -0600

    try fixing seg fault

--- test.asm
+++ test.asm
@@ -1,17 +1,17 @@
-section data
+section .data
     buffer1 db "12345678123456781234567812345678"
     buffer2 db "87654321876543218765432187654321"
     result  times 32 db 0
     multiplier dq 0x40000000, 0x40000000, 0x40000000, 0x40000000, 0x40000000, 0x40000000, 0x40000000, 0x40000000
 
-section text
+section .text
     global _start
 
 _start:
     ; Load addresses of buffers into registers
-    mov rdi, buffer1
-    mov rsi, buffer2
-    mov rdx, result
+    lea rdi, [buffer1]
+    lea rsi, [buffer2]
+    lea rdx, [result]
 
     ; Load data from buffers into ymm registers
     vmovaps ymm0, [rdi]
@@ -21,7 +21,8 @@ _start:
     vaddps ymm2, ymm0, ymm1
 
     ; Multiply the result by a constant (e.g., 2.0)
-    vmovaps ymm3, [multiplier]
+    lea rcx, [multiplier]
+    vmovaps ymm3, [rcx]
     vmulps ymm2, ymm2, ymm3
 
     ; Store the result back into the result buffer
